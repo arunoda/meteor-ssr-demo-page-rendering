bcfcaeb15a57f909ca20e05d43ab70a226ced88d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-11 17:52:22 +0530

    initial commit

---
+++ server/boot_templates.js
@@ -0,0 +1,3 @@
+SSR.compileTemplate('layout', Assets.getText('layout.html'));
+SSR.compileTemplate('home', Assets.getText('home.html'));
+SSR.compileTemplate('singlepost', Assets.getText('singlepost.html'));
